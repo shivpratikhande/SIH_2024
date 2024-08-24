a90f07da9e386dc0dfd37355a40a6078bf356713
Parth Takale <130144456+Parthtakale06@users.noreply.github.com>
2024-08-24 21:55:42 +0530

    created new models

---
+++ models/court-model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const courtSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["District", "High Court", "Supreme Court"],
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Court = mongoose.model("Court", courtSchema);
+module.exports = Court;
