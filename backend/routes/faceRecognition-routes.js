7114cd5083e798809dde37b9368dd5308f9e01bd
shivv <127005505+shivpratikhande@users.noreply.github.com>
2024-08-29 15:20:49 +0530

    Delete backend directory

--- backend/routes/faceRecognition-routes.js
+++
@@ -1,9 +0,0 @@
-import {Router}  from "express";
-
-import * as FaceRecognitionController from "../controllers/face-controller.js"
-import { jwtAuthMiddleware } from "../middlewares/middleware.js";
-
-const router = Router();
-
-router.post("/face-recognition", jwtAuthMiddleware, FaceRecognitionController.faceRecognition );
-export default router;
